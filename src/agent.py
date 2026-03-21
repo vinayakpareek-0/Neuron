186213173c076f37fe22320797e1ae536271d5b1
vpj16 <vpj162@gmail.com>
2026-03-21 23:22:41 +0530

    feat: add llm config and agent builder with conv memory

---
+++ src/agent.py
@@ -0,0 +1,40 @@
+"""
+    Neuron - AgentBuilder
+    Creates the agent with tools, memory and prompt
+"""
+
+from langchain.agents import AgentExecuter , create_tool_calling_agent
+from langchain_core.prompts import ChatPromptTemplate, MessagesPlaceholder
+from langchain.memory import ConversationBufferWindowMemory
+from src.config import llm , SYSTEM_PROMPT
+
+
+def build_agent(tools: list)-> AgentExecuter:
+    """Build the Neuron agent"""
+
+    # prompt_template
+    prompt = ChatPromptTemplate.from_messages([
+        ("system",SYSTEM_PROMPT),
+        MessagesPlaceholder(variable_name ="chat_history"),
+        ("human", "{input}"),
+        MessagesPlaceholder(variable_name="agent_scratchpad"),
+    ])
+    # memory
+    memory = ConversationBufferWindowMemory(
+        k=10,
+        memory_key = "chat_history",
+        return_messages=True,
+    )
+
+    # agent
+    agent= create_tool_calling_agent(llm , tools , prompt)
+
+    # agent_executor
+
+    return AgentExecutor(
+        agent =agent , 
+        tools =tools , 
+        memory = memory,
+        verbose = False,
+        handle_parsing_errors =True,
+    )
